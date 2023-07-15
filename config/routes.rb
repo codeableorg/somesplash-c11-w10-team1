b01c65dae315644fcd7721d955a4fe6482fd05e7
celso <celso.acd.11@gmail.com>
2023-07-14 21:01:12 -0500

    Add routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,7 @@
 Rails.application.routes.draw do
-  # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
-  # Defines the root path route ("/")
-  # root "articles#index"
+  root "categories#index"
   resources :categories
   resources :photos
   resources :comments
+  get "/search", to: "photos#search"
 end
