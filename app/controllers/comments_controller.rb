ec461b57d5686f7f68db1a9193265134ef57aadc
Katya Anco <ladygetya@gmail.com>
2023-07-15 13:23:39 -0500

    cambios en el controller

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,41 +1,49 @@
 class CommentsController < ApplicationController
-    def new
-      if params[:category_id]
-        @category = Category.find(params[:category_id])
-        @comment = @category.comments.new
-      else
-        @photo = Photo.find(params[:photo_id])
-        @comment = @photo.comments.new
-      end
-    end
-  
-    def create
-      if params[:category_id]
-        @category = Category.find(params[:category_id])
-        @comment = @category.comments.new(comment_params)
-      else
-        @photo = Photo.find(params[:photo_id])
-        @comment = @photo.comments.new(comment_params)
-      end
-  
-      if @comment.save && params[:category_id]
-        redirect_to category_path(@category)
-      elsif @comment.save && params[:photo_id]
-        redirect_to photo_path(@photo)
-      else
-        render :new, status: :unprocessable_entity
-      end
-    end
-  
-    def destroy
-      @comment = Comment.find(params[:category_id])
-      @comment.destroy
-  
-      redirect_to category_path, status: :see_other
-    end
-  
-    def comment_params
-      params.require(:comment).permit(:body, :commentable_id, :commentable_type, :type)
+  before_action :set_category_or_photo, only: [:new, :create]
+  before_action :set_comment, only: [:destroy]
+
+  def new
+    @comment = @commentable.comments.new
+  end
+
+  def create
+    @comment = @commentable.comments.new(comment_params)
+
+    if @comment.save
+      redirect_to_commentable
+    else
+      render :new, status: :unprocessable_entity
     end
   end
-  
+
+  def destroy
+    @comment.destroy
+    redirect_to_commentable
+  end
+
+  private
+
+  def set_category_or_photo
+    if params[:category_id]
+      @commentable = Category.find(params[:category_id])
+    elsif params[:photo_id]
+      @commentable = Photo.find(params[:photo_id])
+    end
+  end
+
+  def set_comment
+    @comment = Comment.find(params[:id])
+  end
+
+  def redirect_to_commentable
+    if @commentable.is_a?(Category)
+      redirect_to category_path(@commentable)
+    elsif @commentable.is_a?(Photo)
+      redirect_to photo_path(@commentable)
+    end
+  end
+
+  def comment_params
+    params.require(:comment).permit(:body, :commentable_id, :commentable_type, :type)
+  end
+end
