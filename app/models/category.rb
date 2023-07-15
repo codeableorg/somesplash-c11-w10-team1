13cc70bbd762e5fa73058cca8220ca92c4046e1a
celso <celso.acd.11@gmail.com>
2023-07-14 21:00:14 -0500

    fix models

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,6 @@
 class Category < ApplicationRecord
-    belongs_to :commentable, polymorphic: true
+    # belongs_to :commentable, polymorphic: true
+    has_many :comments, as: :commentable, dependent: :destroy
     has_many :photos, dependent: :destroy
     validates :name, presence: true, uniqueness: true
 end
--- app/models/photo.rb
+++ app/models/photo.rb
@@ -1,5 +1,8 @@
 class Photo < ApplicationRecord
   belongs_to :category , counter_cache: true
-  belongs_to :commentable, polymorphic: true
+  # belongs_to :commentable, polymorphic: true
+  # has_many :comments , as: :commentable, dependent: :destroy
   validates :title, presence: true, uniqueness: true
+
+  has_one_attached :image
 end
